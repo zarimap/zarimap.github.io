1f22ed47b3749b3a8346fd3da73e1569b6796ffc
Zarimap <x4765811@gmail.com>
2026-03-21 16:08:21 +0900

    Update map-logic.js

--- assets/js/map-logic.js
+++ assets/js/map-logic.js
@@ -2,9 +2,11 @@
 
 // ① 地図の土台を作る（緯度, 経度, ズームレベル）
 const map = L.map('map').setView([35.6895, 139.6917], 13);
-
-L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png', {
-    attribution: '© Stadia Maps, © OpenStreetMap contributors',
+// L.tileLayer の部分をこれに書き換え
+L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
+    attribution: '© OpenStreetMap contributors © CARTO',
+    subdomains: 'abcd',
+    maxZoom: 20
 }).addTo(map);
 
 // ③ この後に fetch(...) などの処理を書く
